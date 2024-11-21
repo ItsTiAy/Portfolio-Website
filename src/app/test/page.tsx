8f7692bb98e4e633600b774e341e3201d9fa111e
Thomas Stanway <thomas.stanway@googlemail.com>
2024-11-21 23:13:14 +0000

    Removed test page.

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import Background from "@/app/components/background";
-
-export default function Page() {
-  return (
-    <div className="relative w-full h-screen flex items-center justify-center">
-      Test
-    </div>
-  );
-}
